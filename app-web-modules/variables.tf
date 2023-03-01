4e0e0efc6073027171fb62970eb6041c01270a19
StanislavGM <126458974+StanislavGM@users.noreply.github.com>
2023-03-01 23:12:34 +0300

    Update variables.tf

--- app-web-modules/variables.tf
+++ app-web-modules/variables.tf
@@ -2,7 +2,9 @@ variable "ami" {}
 variable "web_count" {}
 variable "subnet_id" {}
 variable "identity" {}
-variable "instance_type" {}
+variable "instance_type" {
+  default = "t2.micro"
+}
 variable "vpc_security_group_ids" {
   type = list
 }
